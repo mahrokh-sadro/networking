a02b9092ba4faba7a70d491d239d747e420a597e
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-01 19:43:53 -0500

    setting up and User model

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config({ path: "config/keys.env" });
+}
 const app = express();
 
 app.get("/", (req, res) => {
   res.send(`main`);
 });
 
-const PORT = process.env.PORT || 5000;
+app.use('/users',require("./routes/users"))
+app.use('/auth',require("./routes/auth"))
+app.use('/profile',require("./routes/profile"))
+app.use('/posts',require("./routes/posts"))
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+
+
+
+
+const HTTP_PORT = process.env.PORT || 5000;
+
+app.listen(HTTP_PORT, () => {
+  console.log(`app listening on `);
+
+  mongoose
+    .connect(process.env.MONGO_DB_CONNECTION_STRING)
+    .then(() => {
+      console.log("connected to db " + HTTP_PORT);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
